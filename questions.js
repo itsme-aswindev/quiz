e3faf4234c637ea22e46542d099eeb309d2bd2b9
Aswin <74719827+itsme-aswindev@users.noreply.github.com>
2021-08-22 18:17:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,70 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Hey, How is this working ?",
+    answer: "Great",
+    options: [
+      "Great",
+      "Good",
+      "I have no idea !",
+      "oh.. Bad"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How much Rating do you Give ?",
+    answer: "⭐⭐⭐⭐⭐",
+    options: [
+      "⭐⭐⭐",
+      "⭐⭐⭐⭐⭐",
+      "⭐",
+      "⭐⭐⭐⭐"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Did you like the UI ?",
+    answer: "yep 🎉",
+    options: [
+      "No 😒",
+      "yep 🎉",
+      "Its not that bad 🙂",
+      "you kinda improve it ☺"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Have you Enjoyed it ?",
+    answer: "yep 🎉",
+    options: [
+      "Kind of liked it 🙄",
+      "yep 🎉",
+      "No, not at all 🥱",
+      "I would rather prefer not to say 🙂"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Would you like to conduct a quiz with this webapp?",
+    answer: "I would love to 🤗",
+    options: [
+      "I would love to 🤗",
+      "ya sure 😁",
+      "Kind of 🙄",
+      "No 😒"
+    ]
+  },
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
